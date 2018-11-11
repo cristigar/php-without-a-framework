e50c3da48a07489fa2ce45eddf981299a3992f2f
Cristian Garabagiu <cristian.garabagiu@gmail.com>
2018-11-11 22:24:21 +0200

    Added php-di

--- public/index.php
+++ public/index.php
@@ -1,7 +1,19 @@
 <?php
 declare(strict_tyoes=1);
 
+use App\HelloWorld;
+use DI\ContainerBuilder;
+use function DI\create;
+
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
-$helloWorld = new \App\HelloWorld();
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class),
+]);
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(HelloWorld::class);
 $helloWorld->announce();
