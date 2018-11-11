8a4de6348ce362c9f400835022589b7c73244345
Cristian Garabagiu <cristian.garabagiu@gmail.com>
2018-11-11 22:05:55 +0200

    Configured composer autoloader

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
 declare(strict_tyoes=1);
 
-echo 'Hello, world!';
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+
+$helloWorld = new \App\HelloWorld();
+$helloWorld->announce();
